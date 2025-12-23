e47a618a71c5938ebd9d21a5394609dbe30917fa
Joao Victor Conde <jcondedev@gmail.com>
2025-12-23 20:46:20 -0300

    Dia 2 - Lógica basica

--- Semana1/dia2_logica.py
+++ Semana1/dia2_logica.py
@@ -1,1 +1,10 @@
-print("Hello, World!")
+valor = int(input("Digite um número: "))
+soma = 0
+
+while valor != 0 :
+
+    soma = soma + valor 
+
+    valor = int(input("Digite um número: "))
+
+print(soma)
