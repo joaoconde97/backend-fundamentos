074a95a0ae7e1951cacc42cb1e385204d76843d0
Joaoconde <joaotheconde@gmail.com>
2026-01-05 21:39:03 -0300

    Aprendendo Menu + Validação

--- Semana1/dia4_estruturas.py
+++ Semana1/dia4_estruturas.py
@@ -13,15 +13,40 @@ def calcular_saldo(lista):
         total += valor
     return total
 
-valor = int(input("Digite um valor: "))
 
-while valor != 0:
+while  True:
+    print("*********************")
+    print("MENU")
 
-    add_lista(valor)
-    valor = int(input("Digite um valor: "))
+    print("1 - Adicionar Valor")
+    print("2 - Mostrar Saldo")
+    print("0 - Sair")
+    
+
+    opcao = input("Digite a opção desejada: ")
+    print("*********************")
+    opcao = opcao.strip().lower()
 
 
-#Printando resultados
-resultado = calcular_saldo(lista)
-print(f"Lista = {lista}")
-print(f"Soma total da lista = {resultado}")
+    if opcao == "1":
+        valor = input("Digite um valor: ")
+        valor = valor.strip().lower()
+        
+        if valor.isdigit():
+            valor_int = int(valor)
+            add_lista(valor_int)
+        else:
+            print("Valor Inválido")
+
+    elif opcao == "2":
+        resultado = calcular_saldo(lista)
+        print("*********************")
+        print(f"Soma total da lista = {resultado}")
+
+    elif opcao == "0":
+        break
+
+    else:
+        print("Opção Inválida")
+            
+
